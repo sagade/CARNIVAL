db3656e928cc45a3757e9c37ed3e2192161ebe20
enio23 <enio.gjerga@gmail.com>
2019-08-05 14:51:19 +0200

    Update example.R
    Former-commit-id: 37badfcb23eeb324a5b6d07c8170451f225ded22
    Former-commit-id: f1c1a631f47da6a11a12600b1ab3d78a27c572a2

--- CBC-Examples/example.R
+++ CBC-Examples/example.R
@@ -16,4 +16,4 @@ netFile = "Omnipath_signed_Uniprot_0615.txt"
 weightFile = "Ex3_weights_APAP_TGG_24hrHighDose.txt"
 Result_dir = "APAP_Result"
 
-results = runCARNIVAL(solverPath = "~/Desktop/cbc", netFile = netFile, measFile = measFile, weightFile = weightFile, CARNIVAL_example = NULL, timelimit = 3600, mipGAP = 0, Result_dir = Result_dir, solver = "cbc", inverseCR = TRUE)
+results = runCARNIVAL(solverPath = "~/Desktop/cbc", netFile = netFile, measFile = measFile, weightFile = weightFile, CARNIVAL_example = NULL, timelimit = 10000, mipGAP = 0, Result_dir = Result_dir, solver = "cbc", inverseCR = TRUE)
